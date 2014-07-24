4af81e253ee7f0c716b04c02b5756f9212763e00
Yu-Jie Lin <livibetter@gmail.com>
2014-07-24 23:42:42 +0800

    use Autotest to test multiple inputs

--- td-test.sh
+++ td-test.sh
@@ -2,29 +2,6 @@
 
 TD="src/td"
 
-test_counts=0
-test_failures=0
-
-test_eq() {
-  ((test_counts++))
-  test_val="$1"
-  test_ans="$2"
-  shift 2
-  result="$("$TD" "$@" -- $test_val)"
-  if [[ "$result" == "$test_ans" ]]; then
-    printf "Passed: %10s %-10s => %q\n" "$test_val" "$*" "$result"
-    return 0
-  else
-    ((test_failures++))
-    printf "Failed: %10s %-10s => %q != %q\n" "$test_val" "$*" "$result" "$test_ans" >&2
-    return 1
-  fi
-  }
-
-test_eq "1 60" $'1 second \n1 minute ' -P
-
-echo "$test_failures failures of $test_counts tests."
-
 # Performance test
 
 trap 'kill $tmpcount_pid ; rm "$tmpcount" ; exit 130' INT
